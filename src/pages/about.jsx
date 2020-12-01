839ad1e4fe444638f5e308f43b68bd7528538102
Josh <joshua.kearney@verafin.com>
2020-11-30 21:36:32 -0330

    sync remote to local

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => (
+  <div>
+    <h1>
+      About us page
+    </h1>
+    <Link to="/"> Back to Home</Link>
+  </div>
+);
+
+export default AboutPage;
