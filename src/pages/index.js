1d6d5d2c780ec69b5da97b717a8ca59e2a28349d
Josh <joshua.kearney@verafin.com>
2020-12-17 23:08:47 -0330

    remove router

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,44 +4,41 @@ import Layout from "../components/layout"
 import Work from "../components/cards/work";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import {HashRouter, Link} from "react-router-dom";
 import './index.css'
 import Projects from "../components/cards/projects";
 
 const IndexPage = () => {
     return (
-        <HashRouter hashType="noslash">
-            <Layout style={{background: 'rgb(20, 35, 75)'}}>
-                {/*<Blog/>*/}
-                <Grid container
-                      spacing={6}
-                      justify="center"
-                      alignContent="center"
-                      style={{
-                          background: '#ffffff',
-                          height: '4rem',
-                          marginBottom: '3rem'
-                }}>
-                    <Grid item >
-                        <a href="#work" style={{ textDecoration: 'none' }}>
-                            <Typography variant="h5" style={{color: '#1b3c95', fontFamily: 'Open Sans'}}>
-                                Work
-                            </Typography>
-                        </a>
-                    </Grid>
-                    <Grid item>
-                        <a href="#project" style={{ textDecoration: 'none' }}>
-                            <Typography variant="h5" style={{color: '#1b3c95', fontFamily: 'Open Sans'}}>
-                                Projects
-                            </Typography>
-                        </a>
-                    </Grid>
+        <Layout style={{background: 'rgb(20, 35, 75)'}}>
+            {/*<Blog/>*/}
+            <Grid container
+                  spacing={6}
+                  justify="center"
+                  alignContent="center"
+                  style={{
+                      background: '#ffffff',
+                      height: '4rem',
+                      marginBottom: '3rem'
+            }}>
+                <Grid item >
+                    <a href="#work" style={{ textDecoration: 'none' }}>
+                        <Typography variant="h5" style={{color: '#1b3c95', fontFamily: 'Open Sans'}}>
+                            Work
+                        </Typography>
+                    </a>
                 </Grid>
-                <Work />
-                <Projects />
+                <Grid item>
+                    <a href="#project" style={{ textDecoration: 'none' }}>
+                        <Typography variant="h5" style={{color: '#1b3c95', fontFamily: 'Open Sans'}}>
+                            Projects
+                        </Typography>
+                    </a>
+                </Grid>
+            </Grid>
+            <Work />
+            <Projects />
 
-            </Layout>
-        </HashRouter>
+        </Layout>
     )
 }
 
